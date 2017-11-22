5599e2699aa064d9c6b80981f24d6468773a2076
Carlos Henrique Rocha Gabriel <carloshenriquerg@gmail.com>
2017-11-22 19:28:54 -0200

    login

---
+++ BaseApp/BaseApp.Core/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseApp.Core.ViewModels.Base
+{
+    public class ViewModelBase : BindableBase, INavigationAware
+    {
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+            
+        }
+    }
+}
---
+++ BaseApp/BaseApp.Core/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BaseApp.Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseApp.Core.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+    }
+}
